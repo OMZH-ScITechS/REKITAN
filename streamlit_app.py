fadc3b7d58ca5f259ae01577b5d19e5132e3d63e
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-04 09:34:21 +0000

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,39 @@
-# Streamlitライブラリをインポート
 import streamlit as st
+st.title(":red[歴史単語]$$writter$$")
+st.write("$こんにちは。皆さん！今日も元気ですか？ $")
+st.write("$私は元気です！さあ勉強を開始しましょう！$")
+if st.button("$Let's play study!$"):
+    st.write("_the new game!!_")
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+@st.cache
+def load_data():
+    return pd.read_excel("歴史単語～世界史～.xlsx")
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+words_df = load_data()
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# ガチャ機能
+if st.button('ガチャを引く！'):
+    rarity_probs = {
+        'N': 0.4,
+        'R': 0.3,
+        'SR': 0.2,
+        'SSR': 0.1
+    }
+    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
+    subset_df = words_df[words_df['レア度'] == chosen_rarity]
+    selected_word = subset_df.sample().iloc[0]
+    
+    # セッションステートに選択された単語を保存
+    st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
+if 'selected_word' in st.session_state:
+    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+    st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
 
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
+    # 意味を確認するボタンを追加
+    if st.button('意味を確認する'):
+        st.session_state.display_meaning = True
 
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+    if st.session_state.display_meaning:
+        st.write(f"意味: {st.session_state.selected_word['意味']}")
